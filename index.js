bf4553ac4a98edd5b688a088295f7bd83dc2aaca
amanfangeria980 <amanfangeria980@gmail.com>
2023-11-30 23:31:04 +0530

    Mongo atlas connected,env added, and post-job route working

--- index.js
+++ index.js
@@ -1,9 +1,78 @@
 const express=require('express');
 const app=express();
+const cors=require('cors')
 const port=process.env.PORT || 3000;
+const dotenv=require('dotenv');
+dotenv.config();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@job-portal-db.7f2obrh.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    // create db
+    const db=client.db("mernJobPortal");
+    const jobsCollections=db.collection("demoJobs");
+    
+    // post a job
+    app.post("/post-job",async(req,res)=>{
+        const body=req.body;
+        body.createdAt=new Date();
+        // console.log(body)
+        const result=await jobsCollections.insertOne(body);
+        if(result.insertedId){
+            return res.status(200).send(result);
+        }
+        else{
+            return res.status(404).send({
+                message: "Cannot insert! try again later",
+                status: false
+            })
+        }
+    })
+
+
+    // get all jobs
+    app.get("/all-jobs",async(req,res)=>{
+        const jobs=await jobsCollections.find().toArray();
+        res.send(jobs);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get("/",(req,res)=>{
-    res.send("Hello Word!")
+    res.send("Hello World!")
 })
 
 app.listen(port,()=>{
