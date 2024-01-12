282aca3c5f0cd23643904da618bfb255a8548379
amanfangeria980 <amanfangeria980@gmail.com>
2024-01-13 03:53:46 +0530

    my-jobs section handlers added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@job-portal-db.7f2obrh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -56,6 +56,20 @@ async function run() {
         res.send(jobs);
     })
 
+    // get jobs by email
+    app.get("/my-jobs/:email",async(req,res)=>{
+      // console.log(req.params.email);
+      const jobs=await jobsCollections.find({postedBy : req.params.email}).toArray();
+      res.send(jobs);
+    })
+
+    // delete a job
+    app.delete("/job/:id",async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id: new ObjectId(id)}
+      const result=await jobsCollections.deleteOne(filter);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
